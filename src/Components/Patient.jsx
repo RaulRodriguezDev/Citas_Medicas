682101bece863223847895a1f98a3db22c9f2057
Raul Rodriguez <raul.rodriguez.dev@gmail.com>
2023-01-23 22:57:48 -0600

    Doing read performance

--- src/Components/Patient.jsx
+++ src/Components/Patient.jsx
@@ -1,30 +1,34 @@
-const Patient = () => { 
+const Patient = ({patient}) => {
+
+  const {name,owner,email,date,symptoms} = patient
+
   return (
     <div className="bg-white m-3 shadow-md px-5 py-10 rounded-xl">
-    <p className="uppercase font-bold mb-3 text-gray-700">
-        Name:{" "}
-      <span className="normal-case font-normal">Hook</span>
-    </p>
-    <p className="uppercase font-bold mb-3 text-gray-700">
-      Owner:{" "}
-      <span className="normal-case font-normal">Juan</span>
-    </p>
-    <p className="uppercase font-bold mb-3 text-gray-700">
-      Email:{" "}
-      <span className="normal-case font-normal">correo@email.com</span>
-    </p>
-    <p className="uppercase font-bold mb-3 text-gray-700">
-      Discharge Date:{" "}
-      <span className="normal-case font-normal">January 23 2023</span>
-    </p>
-    <p className="uppercase font-bold mb-3 text-gray-700">
-      Symptoms:{" "}
-      <span className="normal-case font-normal">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam aut necessitatibus nostrum tempore aliquam magnam
-        odio id cupiditate nesciunt quas, suscipit accusamus cum nulla, alias perferendis, nobis perspiciatis eaque? 
-        Corrupti!
-      </span>
-    </p>
+      <p className="uppercase font-bold mb-3 text-gray-700">
+          Name:{" "}
+        <span className="normal-case font-normal">{name}</span>
+      </p>
+      <p className="uppercase font-bold mb-3 text-gray-700">
+        Owner:{" "}
+        <span className="normal-case font-normal">{owner}</span>
+      </p>
+      <p className="uppercase font-bold mb-3 text-gray-700">
+        Email:{" "}
+        <span className="normal-case font-normal">{email}</span>
+      </p>
+      <p className="uppercase font-bold mb-3 text-gray-700">
+        Discharge Date:{" "}
+        <span className="normal-case font-normal">{date}</span>
+      </p>
+      <p className="uppercase font-bold mb-3 text-gray-700">
+        Symptoms:{" "}
+        <span className="normal-case font-normal">
+          {symptoms}
+        </span>
+      </p>
+      <div className="flex justify-between mt-10">
+
+      </div>
   </div>
   )
 }
--- src/Components/PatientList.jsx
+++ src/Components/PatientList.jsx
@@ -1,21 +1,26 @@
 import Patient from "./Patient"
 
-function PatientList() {
+function PatientList({patients}) {
   return (
     <div className="md:w-1/2 lg:w-3/5 h-screen md:overflow-y-scroll">
-      <h2 className="font-black text-center text-3xl">Patient List</h2>
-      <p className="text-xl mt-5 mb-10 text-center">
-        Admin your {""}
-        <span className="text-indigo-600 font-bold">Patients and appointments</span>
-      </p>
-      <Patient/>
-      <Patient/>
-      <Patient/>
-      <Patient/>
-      <Patient/>
-      <Patient/>
-      <Patient/>
-      <Patient/>
+      {patients && patients.lenght ? (
+        <>
+          <h2 className="font-black text-center text-3xl">Patient List</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Admin your {""}
+            <span className="text-indigo-600 font-bold">Patients and appointments</span>
+          </p>
+          {patients.map(patient => <Patient key={patient.id} patient={patient}/>)}
+        </>
+      ):(
+        <>
+          <h2 className="font-black text-center text-3xl">Neither patient yet</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Begin adding your patients and {""}
+            <span className="text-indigo-600 font-bold">Will displayed here</span>
+          </p>
+      </>
+      )}
     </div>
   )
 }
