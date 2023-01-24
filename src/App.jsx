97c27c89c33bf86213ea4de8caf8ab7c8e36fe65
Raul Rodriguez <raul.rodriguez.dev@gmail.com>
2023-01-23 22:56:45 -0600

    Doing create performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
+import { useState } from "react"
 import Form from "./Components/Form"
 import Header from "./Components/Header"
 import PatientList from "./Components/PatientList"
 
 function App() {
 
+  const [patients,setPatients] = useState([])
+
   return (
     <div className='container mx-auto mt-20'>
       <Header/>
       <div className='mt-12 md:flex'>
-      <Form/>
-      <PatientList/>
+      <Form  patients={patients} setPatients={setPatients}/>
+      <PatientList patients={patients}/>
       </div>
     </div>
   )
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,11 +1,57 @@
-function Form() {
+import { useState , useEffect} from "react"
+import Error from "./Error"
+
+const generateId = () => {
+        const random = Math.random().toString(32).substring(2)
+        const date = Date.now().toString(32)
+
+        return date + random
+}
+function Form({patients,setPatients}) {
+
+        const [name,setName] = useState("")
+        const [owner,setOwner] = useState("")
+        const [email,setEmail] = useState("")
+        const [date,setDate] = useState("")
+        const [symptoms,setSymptoms] = useState("")
+
+        const [error,setError] = useState(false)
+
+        const handleSubmit = (e) => {
+
+                e.preventDefault()
+
+                if([name,owner,email,date,symptoms].includes('')){
+                        setError(true)
+                        return;
+                }
+
+                const objPatient = {
+                        name,
+                        owner,
+                        email,
+                        date,
+                        symptoms,
+                        id: generateId()
+                }
+
+                setPatients([...patients,objPatient])
+                setName('')
+                setOwner('')
+                setEmail('')
+                setDate('')
+                setSymptoms('')
+                setError(false)
+        }
+
   return (
     <div className='md:w-1/2 lg:w-2/5 mx-5'>
-      <h2 class='font-black text-center text-3xl'>Patient Tracking</h2>
+      <h2 className='font-black text-center text-3xl'>Patient Tracking</h2>
       <p className="text-lg mt-5 text-center mb-10">
         Add patient and {""} <span className='font-bold text-indigo-600'>Admin</span>
       </p>
-      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10" onSubmit={handleSubmit}>
+        {error && <Error><p>All fields are required</p></Error>}
         <div className="mb-5">
             <label htmlFor='pet' className="block uppercase text-gray-700 font-bold">Pet Name</label>
             <input 
@@ -13,6 +59,8 @@ function Form() {
                     type="text" 
                     placeholder="Pet Name"
                     className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
             />
         </div>
         <div className="mb-5">
@@ -22,6 +70,8 @@ function Form() {
                     type="text" 
                     placeholder="Owner Name"
                     className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    value={owner}
+                    onChange={(e) => setOwner(e.target.value)}
             />
         </div>
         <div className="mb-5">
@@ -31,6 +81,8 @@ function Form() {
                     type="email" 
                     placeholder="Owner's email contact"
                     className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
             />
         </div>
         <div className="mb-5">
@@ -39,6 +91,8 @@ function Form() {
                     id="discharge_date"
                     type="date" 
                     className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    value={date}
+                    onChange={(e) => setDate(e.target.value)}
             />
         </div>
         <div className="mb-5">
@@ -47,6 +101,8 @@ function Form() {
                     id="symptoms" 
                     placeholder="Describe the symptoms"
                     className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    value={symptoms}
+                    onChange={(e) => setSymptoms(e.target.value)}
             />
         </div>
         <input 
