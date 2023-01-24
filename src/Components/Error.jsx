54f121200e7ca10cd560d0e7c56f1d3b12330b07
Raul Rodriguez <raul.rodriguez.dev@gmail.com>
2023-01-24 01:47:19 -0600

    Minor changes before deployment

---
+++ src/Components/Error.jsx
@@ -0,0 +1,9 @@
+const Error = ({children}) => {
+  return (
+    <div className=" bg-red-800 rounded-md text-white font-bold mb-3 uppercase text-center p-3">
+      {children}
+    </div>
+  )
+}
+
+export default Error
--- src/Components/PatientList.jsx
+++ src/Components/PatientList.jsx
@@ -1,16 +1,17 @@
 import Patient from "./Patient"
 
-function PatientList({patients}) {
+function PatientList({patients,setPatient, deletePatient}) {
+
   return (
     <div className="md:w-1/2 lg:w-3/5 h-screen md:overflow-y-scroll">
-      {patients && patients.lenght ? (
+      {patients && patients.length ? (
         <>
           <h2 className="font-black text-center text-3xl">Patient List</h2>
           <p className="text-xl mt-5 mb-10 text-center">
             Admin your {""}
             <span className="text-indigo-600 font-bold">Patients and appointments</span>
           </p>
-          {patients.map(patient => <Patient key={patient.id} patient={patient}/>)}
+          {patients.map(patient => <Patient key={patient.id} patient={patient} setPatient={setPatient} deletePatient={deletePatient}/>)}
         </>
       ):(
         <>
@@ -19,7 +20,7 @@ function PatientList({patients}) {
             Begin adding your patients and {""}
             <span className="text-indigo-600 font-bold">Will displayed here</span>
           </p>
-      </>
+        </>
       )}
     </div>
   )
